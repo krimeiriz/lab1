5a6f78d4e312498af93269548670fd5e24e650a7
krimeiriz <367296@niuitmo.ru>
2023-11-24 02:12:57 +0600

    create project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        
+    }
+}
